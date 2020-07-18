353c797ac83148cfb1f8113ff1c4c3f2d7aebd35
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-18 10:14:31 +0900

    add Project 14, part 1
    https://www.hackingwithswift.com/100/swiftui/68

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,9 +8,103 @@
 
 import SwiftUI
 
-struct ContentView: View {
+//struct ContentView: View {
+//    let values = [1, 5, 3, 6, 2, 9].sorted()
+//
+//    var body: some View {
+//        List(values, id: \.self) {
+//            Text(String($0))
+//        }
+//    }
+//}
+
+//struct User: Identifiable, Comparable {
+//    let id = UUID()
+//    let firstName: String
+//    let lastName: String
+//
+//    static func < (lhs: User, rhs: User) -> Bool {
+//        lhs.lastName < rhs.lastName
+//    }
+//}
+//
+//struct ContentView: View {
+//    let users = [
+//        User(firstName: "Arnold", lastName: "Rimmer"),
+//        User(firstName: "Kristine", lastName: "Kochanski"),
+//        User(firstName: "David", lastName: "Lister"),
+//    ].sorted()
+//
+//    var body: some View {
+//        List(users) { user in
+//            Text("\(user.lastName), \(user.firstName)")
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//
+//    var body: some View {
+//        Text("Hello World")
+//            .onTapGesture {
+//                let str = "Test Message"
+//                let url = self.getDocumentsDirectory().appendingPathComponent("message.txt")
+//
+//                do {
+//                    try str.write(to: url, atomically: true, encoding: .utf8)
+//                    let input = try String(contentsOf: url)
+//                    print(input)
+//                } catch {
+//                    print(error.localizedDescription)
+//                }
+//        }
+//    }
+//
+//    func getDocumentsDirectory() -> URL {
+//        // find all possible documents directories for this user
+//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//
+//        // just send back the first one, which ought to be the only one
+//        return paths[0]
+//    }
+//}
+
+struct LoadingView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+struct ContentView: View {
+    
+    var loadingState = LoadingState.loading
+    
+    enum LoadingState {
+        case loading, success, failed
+    }
+
+    var body: some View {
+        Group {
+            if loadingState == .loading {
+                LoadingView()
+            } else if loadingState == .success {
+                SuccessView()
+            } else if loadingState == .failed {
+                FailedView()
+            }
+        }
     }
 }
 
